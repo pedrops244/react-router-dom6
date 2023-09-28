2318b62e98a902245116a75c87370d31accf1d33
Pedro Souza <pedrops244@hotmail.com>
2023-09-28 19:28:29 -0300

    Menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,16 @@
+import './style.css';
+import { Link } from 'react-router-dom';
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './styles/GlobalStyles.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Menu } from './components/Menu';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
+    <Menu />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
