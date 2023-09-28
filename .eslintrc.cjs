1d7fe8c05452e13046423a8bf8dd23dea432233e
Pedro Souza <pedrops244@hotmail.com>
2023-09-28 19:15:59 -0300

    BrowserRoutes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,17 @@ module.exports = {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+
     ],
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-console': 0,
+    'default-param-last': 0,
+    'react/jsx-no-bind': 0,
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './styles/GlobalStyles.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
+import { About } from './components/About';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 );
